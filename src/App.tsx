b8a952360c7f06e6408b99c91b4d513864ea4a4f
Petr Janik <485122@mail.muni.cz>
2020-11-28 12:13:37 +0100

    Fetch and print raw data

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const fetchData = async () => {
+  const response = await axios.get(
+    "https://search-api.fie.future.net.uk/widget.php?id=review&model_name=xbox_one_s&area=GB"
+  );
+  return response.data;
+};
 
 const App = () => {
-    return (
-        <h1>Hello World</h1>
-    );
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    fetchData().then((data) => setData(data));
+  }, []);
+
+  return <pre>{JSON.stringify(data, null, 2)}</pre>;
 };
 
 export default App;
