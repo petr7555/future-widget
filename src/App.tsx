2afde1f858e977350a38c1aff4d05233bc69d3d9
Petr Janik <485122@mail.muni.cz>
2020-11-28 13:33:31 +0100

    Show item data in table

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,56 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-const fetchData = async () => {
+const NUMBER_OF_ITEMS = 4;
+
+const fetchItems = async () => {
   try {
     const { data } = await axios.get(
       "https://search-api.fie.future.net.uk/widget.php?id=review&model_name=xbox_one_s&area=GB"
     );
-    return data;
+    return data.widget.data.offers.slice(0, NUMBER_OF_ITEMS);
   } catch (error) {
     console.log(error);
   }
 };
 
 const App = () => {
-  const [data, setData] = useState();
+  const [items, setItems] = useState();
 
   useEffect(() => {
-    fetchData().then((data) => setData(data));
+    fetchItems().then((offers) => setItems(offers));
   }, []);
 
-  return <pre>{JSON.stringify(data, null, 2)}</pre>;
+  if (!items) {
+    return null;
+  }
+
+  return (
+    <table>
+      {
+        // @ts-ignore
+        items.map((item) => (
+          <tr key={item.id}>
+            <td>
+              <img src={item.image} alt={item.name} />
+            </td>
+            <td>{item.offer.name}</td>
+            <td>
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: `${item.offer.currency_symbol}`,
+                }}
+              />
+              {`${item.offer.price} at ${item.merchant.name}`}
+            </td>
+            <td>
+              <a href={item.offer.link}>{item.offer.link_text}</a>
+            </td>
+          </tr>
+        ))
+      }
+    </table>
+  );
 };
 
 export default App;
