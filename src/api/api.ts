a2459a34a54c0cdca2c3dc34263ed5f0a6667616
Petr Janik <485122@mail.muni.cz>
2020-12-04 09:48:12 +0100

    Extract fetch into separate file

---
+++ src/api/api.ts
@@ -0,0 +1,28 @@
+import axios from "axios";
+import {Item} from "../types/types";
+
+const NUMBER_OF_ITEMS = 4;
+
+const createItem = (rawItem: any): Item => ({
+    id: rawItem.id,
+    name: rawItem.offer.name,
+    imageUrl: rawItem.image,
+    currencySymbol: rawItem.offer.currency_symbol,
+    price: rawItem.offer.price,
+    merchantName: rawItem.merchant.name,
+    link: rawItem.offer.link,
+    linkText: rawItem.offer.link_text,
+});
+
+export const fetchItems = async () => {
+    try {
+        const { data } = await axios.get(
+            "https://search-api.fie.future.net.uk/widget.php?id=review&model_name=xbox_one_s&area=GB"
+        );
+        const rawItems: any[] = data.widget.data.offers.slice(0, NUMBER_OF_ITEMS);
+        return rawItems.map((rawItem) => createItem(rawItem));
+    } catch (e) {
+        console.log(e);
+        return e;
+    }
+};
--- src/components/Offers.tsx
+++ src/components/Offers.tsx
@@ -1,34 +1,8 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
 import { Item } from "../types/types";
 import SingleOffer from "./SingleOffer";
 import styled from "styled-components";
-
-const NUMBER_OF_ITEMS = 4;
-
-const fetchItems = async () => {
-  try {
-    const { data } = await axios.get(
-      "https://search-api.fie.future.net.uk/widget.php?id=review&model_name=xbox_one_s&area=GB"
-    );
-    const rawItems: any[] = data.widget.data.offers.slice(0, NUMBER_OF_ITEMS);
-    return rawItems.map((rawItem) => createItem(rawItem));
-  } catch (e) {
-    console.log(e);
-    return e;
-  }
-};
-
-const createItem = (rawItem: any): Item => ({
-  id: rawItem.id,
-  name: rawItem.offer.name,
-  imageUrl: rawItem.image,
-  currencySymbol: rawItem.offer.currency_symbol,
-  price: rawItem.offer.price,
-  merchantName: rawItem.merchant.name,
-  link: rawItem.offer.link,
-  linkText: rawItem.offer.link_text,
-});
+import {fetchItems} from "../api/api";
 
 const Container = styled.div`
   border-radius: 15px;
